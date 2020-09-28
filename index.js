586bdc2e70315f2dcbde05c54205226d0c4f611f
beer <bear_za-2555@hotmail.com>
2020-09-28 10:45:39 +0700

    beer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hbs = require('express-handlebars');
 const router = require('./routes/index');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3080;
 
 app.use(express.static(path.join(__dirname,'public')));
 app.engine('hbs',hbs({extname:'hbs'}));
